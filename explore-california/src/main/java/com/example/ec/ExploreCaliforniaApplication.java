9de4e1137f09376cda155da91034f2fedbadc4e9
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-10-21 16:29:33 -0500

    starting projects

---
+++ explore-california/src/main/java/com/example/ec/ExploreCaliforniaApplication.java
@@ -0,0 +1,12 @@
+package com.example.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliforniaApplication.class, args);
+	}
+}
