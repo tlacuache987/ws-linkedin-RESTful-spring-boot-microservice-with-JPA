9a410c03279d4ea805ee64eb400515f0adee7888
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-10-23 21:21:51 -0500

    adding @Param for search endpoints

--- explore-california/src/main/java/com/example/ec/repositories/TourPackageRepository.java
+++ explore-california/src/main/java/com/example/ec/repositories/TourPackageRepository.java
@@ -1,11 +1,12 @@
 package com.example.ec.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.ec.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
-	TourPackage findByName(String name);
+	TourPackage findByName(@Param("name")String name);
 
 }
--- explore-california/src/main/java/com/example/ec/repositories/TourRepository.java
+++ explore-california/src/main/java/com/example/ec/repositories/TourRepository.java
@@ -1,9 +1,13 @@
 package com.example.ec.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.ec.domain.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
 
+	List<Tour> findByTourPackageCode(@Param("code") String code);
 }
