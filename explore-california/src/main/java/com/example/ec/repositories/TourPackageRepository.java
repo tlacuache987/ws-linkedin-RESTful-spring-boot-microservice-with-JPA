c563e45d61958788c817eb33fb888accd6efff26
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-10-23 21:44:39 -0500

    adding support for control rest respurce exposure and change rel an path of a rest resource

--- explore-california/src/main/java/com/example/ec/repositories/TourPackageRepository.java
+++ explore-california/src/main/java/com/example/ec/repositories/TourPackageRepository.java
@@ -2,11 +2,40 @@ package com.example.ec.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.domain.TourPackage;
 
+// annotation at class level
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
-	TourPackage findByName(@Param("name")String name);
+	TourPackage findByName(@Param("name") String name);
+
+	@Override
+	// annotation at method annotation
+	@RestResource(exported = false)
+	<S extends TourPackage> S save(S entity);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends TourPackage> Iterable<S> save(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(String id);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(TourPackage entity);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Iterable<? extends TourPackage> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll();
 
 }
--- explore-california/src/main/java/com/example/ec/repositories/TourRepository.java
+++ explore-california/src/main/java/com/example/ec/repositories/TourRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.domain.Tour;
 
@@ -15,4 +16,30 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Integer
 	// size = defaults 20 &
 	// sort = entity Id, asc
 	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+	@Override
+	// annotation at method annotation
+	@RestResource(exported = false)
+	<S extends Tour> S save(S entity);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Tour> Iterable<S> save(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Integer id);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Tour entity);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Iterable<? extends Tour> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll();
+
 }
